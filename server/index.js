56c9e857804618cd7a195b656282f33c15ca8629
Etienne <etiennecclarke@icloud.com>
2023-07-14 12:19:31 +0100

    Updated port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // Note that this file isn't processed by Vite, see https://github.com/brillout/vite-plugin-ssr/issues/562
 
-const express = require('express')
+const express = require('express') 
 const compression = require('compression')
 const { renderPage } = require('vite-plugin-ssr/server')
 
@@ -10,38 +10,38 @@ const root = `${__dirname}/..`
 startServer()
 
 async function startServer() {
-  const app = express()
+	const app = express()
 
-  app.use(compression())
+	app.use(compression())
 
-  if (isProduction) {
-    const sirv = require('sirv')
-    app.use(sirv(`${root}/dist/client`))
-  } else {
-    const vite = require('vite')
-    const viteDevMiddleware = (
-      await vite.createServer({
-        root,
-        server: { middlewareMode: true }
-      })
-    ).middlewares
-    app.use(viteDevMiddleware)
-  }
+	if (isProduction) {
+		const sirv = require('sirv')
+		app.use(sirv(`${root}/dist/client`))
+	} else {
+		const vite = require('vite')
+		const viteDevMiddleware = (
+		await vite.createServer({
+			root,
+			server: { middlewareMode: true }
+		})
+		).middlewares
+		app.use(viteDevMiddleware)
+	}
 
-  app.get('*', async (req, res, next) => {
-    console.log(`Received a ${req.method} request from '${req.socket.remoteAddress}' for ${req.url}`);
-    const pageContextInit = {
-      urlOriginal: req.originalUrl
-    }
-    const pageContext = await renderPage(pageContextInit)
-    const { httpResponse } = pageContext
-    if (!httpResponse) return next()
-    const { body, statusCode, contentType, earlyHints } = httpResponse
-    if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
-    res.status(statusCode).type(contentType).send(body)
-  })
+	app.get('*', async (req, res, next) => {
+		console.log(`Received a ${req.method} request from '${req.socket.remoteAddress}' for ${req.url}`);
+		const pageContextInit = {
+			urlOriginal: req.originalUrl
+		}
+		const pageContext = await renderPage(pageContextInit)
+		const { httpResponse } = pageContext
+		if (!httpResponse) return next()
+		const { body, statusCode, contentType, earlyHints } = httpResponse
+		if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+		res.status(statusCode).type(contentType).send(body)
+	});
 
-  const port = process.env.PORT || 80
-  app.listen(port)
-  console.log(`Server running on port: ${port}`)
+	const port = 8000;
+	app.listen(port);
+	console.log(`Server running on port: ${port}`);
 }
